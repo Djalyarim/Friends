952176d601f7df4f3764f0c73bb9ca887ac93c24
Максим Кузьмин <Djalyarim@gmail.com>
2021-03-28 00:37:44 +0500

    Third try

--- posts/forms.py
+++ posts/forms.py
@@ -9,10 +9,7 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('group', 'text', 'image')
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 60bbec9238e8b566653de0fdb1132a9f88c60c0e
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
@@ -20,7 +17,3 @@ class CommentForm(forms.ModelForm):
         widgets = {
             'text': Textarea(attrs={'cols': 80, 'rows': 5}),
         }
-<<<<<<< HEAD
-=======
-
->>>>>>> 60bbec9238e8b566653de0fdb1132a9f88c60c0e
--- yatube/urls.py
+++ yatube/urls.py
@@ -16,12 +16,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-<<<<<<< HEAD
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
-=======
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
->>>>>>> 60bbec9238e8b566653de0fdb1132a9f88c60c0e
