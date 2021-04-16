8ae4c46e81e222ffc27f7192097a738f9714afb3
Максим Кузьмин <Djalyarim@gmail.com>
2021-04-16 15:18:31 +0500

    last version 10

--- users/views.py
+++ users/views.py
@@ -1,10 +1,27 @@
+# from django.core.exceptions import ObjectDoesNotExist
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from .forms import CreationForm
+
+# from posts.models import Profile_id, User
+
 
 
 class SignUpView(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+# @receiver(post_save, sender=User)
+# def save_or_create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile_id.objects.create(user=instance)
+#     else:
+#         try:
+#             instance.profile_id.save()
+#         except ObjectDoesNotExist:
+#             Profile_id.objects.create(user=instance)
